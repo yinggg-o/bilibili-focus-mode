8732417520d8795c121d9c52acdee2fd67bc2262
yinggg-o <Agynessup@gmail.com>
2023-11-22 22:19:35 +0800

    chrome extension version 1.0

--- popup.js
+++
@@ -1,8 +0,0 @@
-document.addEventListener('DOMContentLoaded', function() {
-    var toggleButton = document.getElementById('toggleButton');
-  
-    chrome.tabs.query({ active: true, currentWindow: true }, function(tabs) {
-      var activeTab = tabs[0];
-      chrome.tabs.sendMessage(activeTab.id, { action: 'toggleBlocking' });
-    });
-  });
