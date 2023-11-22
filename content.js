6f8a5e24f75aa66aa42046412bfd12638ddb3c99
yinggg-o <Agynessup@gmail.com>
2023-11-22 09:11:40 +0800

    Finished the architecture yesterday, work on the details today

--- content.js
+++ content.js
@@ -1,67 +1,75 @@
 // content.js
-function hideUnwantedContent() {
-  // Define the selector for the dropdown banner
-  var dropdownBannerSelector = '#your-dropdown-banner-id'; // Replace with your actual selector
 
-  // Hide the dropdown banner
-  var dropdownBanner = document.querySelector(dropdownBannerSelector);
-  if (dropdownBanner) {
-    dropdownBanner.style.display = 'none';
+function hideIrrelevantElements() {
+  // Get the current URL
+  var currentUrl = window.location.href;
+
+  // Define fixed URLs for different pages
+  var homepageUrl = 'https://www.bilibili.com/';
+  var videoWatchPageUrl = 'https://www.bilibili.com/video/';
+  var memberPageUrl = 'https://member.bilibili.com/';
+  var livestreamPageUrl = 'https://live.bilibili.com/';
+
+  // Check the current URL and apply specific styles accordingly
+  if (currentUrl === homepageUrl) {
+    // Set data-page-url attribute for the homepage
+    document.body.setAttribute('data-page-url', homepageUrl);
+
+    // Apply styles for the homepage
+    var homepageSelectors = [
+      '.animated-banner',
+      '.bili-feed4-layout',
+      // Add more selectors for the homepage as needed
+    ];
+
+    hideElements(homepageSelectors);
+  } else if (currentUrl.startsWith(videoWatchPageUrl)) {
+    // Set data-page-url attribute for the video watch page
+    document.body.setAttribute('data-page-url', videoWatchPageUrl);
+
+    // Apply styles for the video watch page
+    var videoWatchPageSelectors = [
+      '.video-player',
+      '.related-videos'
+      // Add more selectors for the video watch page as needed
+    ];
+
+    hideElements(videoWatchPageSelectors);
+  } else if (currentUrl.startsWith(memberPageUrl)) {
+    // Set data-page-url attribute for the member page
+    document.body.setAttribute('data-page-url', memberPageUrl);
+
+    // Apply styles for the member page
+    var memberPageSelectors = [
+      '.sidebar-member-page',
+      '#profile-section',
+      '.member-related-element'
+      // Add more selectors for the member page as needed
+    ];
+
+    hideElements(memberPageSelectors);
+  } else if (currentUrl.startsWith(livestreamPageUrl)) {
+    // Set data-page-url attribute for the livestream page
+    document.body.setAttribute('data-page-url', livestreamPageUrl);
+
+    // Apply styles for the livestream page
+    var livestreamPageSelectors = [
+      '.livestream-sidebar',
+      '.livestream-related-element'
+      // Add more selectors for the livestream page as needed
+    ];
+
+    hideElements(livestreamPageSelectors);
   }
+}
 
-  // Define the selectors for unwanted feeds
-  var unwantedFeedSelectors = [
-    '.bili-feed4-layout',
-    '.left-entry',
-    '.bili-header__banner',
-    '.header-channel-fixed'
-    // Add more selectors as needed
-  ];
-
-  // Hide unwanted feeds
-  function hideElements(elements) {
+function hideElements(selectors) {
+  // Hide elements based on provided selectors
+  selectors.forEach(selector => {
+    var elements = document.querySelectorAll(selector);
     elements.forEach(element => {
       element.style.display = 'none';
     });
-  }
-
-  // Hide unwanted feeds based on defined selectors
-  unwantedFeedSelectors.forEach(selector => {
-    var unwantedFeeds = document.querySelectorAll(selector);
-    hideElements(unwantedFeeds);
   });
 }
 
-// Run the function when the DOM is ready
-if (document.readyState === 'loading') {
-  document.addEventListener('DOMContentLoaded', hideUnwantedContent);
-} else {
-  hideUnwantedContent();
-}
-
-// Set up a MutationObserver to watch for dynamically loaded elements
-var targetNode = document.body;
-var config = { childList: true, subtree: true };
-
-var callback = function (mutationsList, observer) {
-  for (var mutation of mutationsList) {
-    if (mutation.type === 'childList') {
-      // Check if a new node with the dropdown banner selector has been added
-      var addedNodes = Array.from(mutation.addedNodes);
-      var dropdownBanner = addedNodes.find(node => node.matches && node.matches(dropdownBannerSelector));
-      if (dropdownBanner) {
-        // Hide the dynamically loaded dropdown banner
-        dropdownBanner.style.display = 'none';
-      }
-
-      // Check if a new node with an unwanted feed selector has been added
-      unwantedFeedSelectors.forEach(selector => {
-        var unwantedFeeds = addedNodes.filter(node => node.matches && node.matches(selector));
-        hideElements(unwantedFeeds);
-      });
-    }
-  }
-};
-
-var observer = new MutationObserver(callback);
-observer.observe(targetNode, config);
